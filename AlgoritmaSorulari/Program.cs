84116c817dd65fb9e38d47bd3d86a4237b59a006
oguzhandilek <oguzhandilek25@gmail.com>
2024-04-01 05:28:59 +0300

    Soru-Cevap

---
+++ AlgoritmaSorulari/Program.cs
@@ -0,0 +1,155 @@
+﻿using AlgoritmaSorulari;
+using System;
+
+Sorular sorular = new Sorular();
+int ilkSayi = 0;
+int ikinciSayi = 0;
+double topla = default;
+string sonuc = default;
+#region Soru1-Cevap1
+
+
+//Console.WriteLine("İlk Sayıyı Giriniz");
+
+//tryAgain1:
+
+//if (!int.TryParse(Console.ReadLine(), out ilkSayi))
+//{
+//    Console.WriteLine("Lütfen değeri sayısal giriniz");
+//    goto tryAgain1;
+//}
+
+//tryAgain2:
+
+//Console.WriteLine("İkinci Sayıyı Giriniz");
+//if (!int.TryParse(Console.ReadLine(), out ikinciSayi))
+//{
+//    Console.WriteLine("Lütfen sayısal değer giriniz");
+//    goto tryAgain2;
+//}
+
+
+//Console.WriteLine($@"Girilen iki  sayının toplamı: {sorular.Soru(ilkSayi, ikinciSayi)} </>");
+
+
+
+#endregion
+
+#region Soru2-Cevap2
+//tryAgain1:
+//Console.WriteLine("İlk Sayıyı Gİirniz");
+
+//if (!int.TryParse(Console.ReadLine(), out ilkSayi))
+//{
+//    Console.WriteLine("Lütfen sayısal değer giriniz");
+//    goto tryAgain1;
+//}
+//double _ilksSayi = Math.Pow(ilkSayi, 2);
+
+//tryAgain2:
+//Console.WriteLine("İkinci Sayıyı Giriniz");
+//if (!int.TryParse(Console.ReadLine(), out ikinciSayi))
+//{
+//    Console.WriteLine("Lütfen sayısal değer giriniz");
+//    goto tryAgain2;
+//}
+//double _İkinciSayi = Math.Pow(ikinciSayi, 2);
+
+//Console.WriteLine($"{ilkSayi} ile {ikinciSayi} sayılarının kareleri toplamı {sorular.Soru(_ilksSayi, _İkinciSayi)}");
+
+#endregion
+
+#region Soru3-Cevap3
+//For Dögüsü ile
+//for (int i = 1; i <= 10; i++)
+//{
+//    topla += Math.Pow(i, 3);
+
+//}
+//Console.WriteLine(topla);
+
+//While Dögüsü ile
+//string sonuc = default;
+//int sayac = 1;
+//while (true)
+//{
+//    topla += Math.Pow(sayac, 3);
+//    sonuc += $"{sayac}³ + ";
+//    sayac++;
+//    if (sayac == 11)
+//    {
+//        sonuc += $"= {topla}";
+//        break;
+//    }
+
+//}
+//Console.WriteLine(sonuc);
+
+//Do While Döngüsü ile
+
+//do
+//{
+//    topla += Math.Pow(sayac, 3);
+//sayac++;
+//} while (sayac < 11) ;
+//Console.WriteLine($"Toplam: {topla}");
+#endregion
+
+#region Soru4-Cevap4
+
+//Sadece tarih ile
+//int dogumTarihi = 0;
+
+//tryAgain:
+//Console.WriteLine("Doğum tarihinizi yıl olarak giriniz");
+//if (!int.TryParse(Console.ReadLine(),out dogumTarihi))
+//{
+//    Console.WriteLine("Doğum tarihini yıl olarak giriniz örnek:1992");
+//    goto tryAgain;
+//}
+//int yas = DateTime.Now.Year - dogumTarihi;
+//Console.WriteLine($"{yas} yaşındasınız");
+
+//Gün/Ay/Yıl ile
+//tryAgain:
+//Console.WriteLine("Doğum tarihinizi Gün.Ay.Yıl olarak giriniz");
+//DateTime dogumTarihi = new();
+//if (!DateTime.TryParse(Console.ReadLine(), out dogumTarihi))
+//{
+//    Console.WriteLine("Doğum tarhinizi Gün, Ay, Yıl olarak ve aralara nokta koyarak giriniz");
+//    goto tryAgain;
+//}
+//TimeSpan yas = DateTime.Now - dogumTarihi;
+//Console.WriteLine($@"{yas.Days/365} yaşındasınız");
+
+
+#endregion
+
+#region Soru5-Cevap5
+// for döngüsü ile
+//double carp = 1;
+//tryAgain:
+//Console.WriteLine("Bir sayı giriniz");
+//if (!double.TryParse(Console.ReadLine(), out double sayi))
+//{
+//    Console.WriteLine("Lütfen sayısal değer giriniz");
+//    goto tryAgain;
+//}
+//for (int i = 1; i <= sayi; i++)
+//{
+//    carp *= i;
+//    sonuc += $"{i.ToString()}.";
+//    if (i >= sayi)
+//    {
+//        sonuc = sonuc.Remove(sonuc.Length-1);
+//        sonuc += $"= {carp}";
+//    }
+
+
+//}
+//Console.WriteLine(sonuc);
+
+//while döngüsü ile
+
+
+#endregion
---
+++ AlgoritmaSorulari/Sorular.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmaSorulari
+{
+    public class Sorular
+    {
+        #region Soru 1
+        // İki sayının toplanını veren uygulamayı yazınız.
+        public int Soru(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        #endregion
+
+        #region Soru 2
+        //Kullanıcxnın girdiği İki sayının karelerinin toplamını veren uygulanayı yazınız.
+        public double Soru(double sayi1, double sayi2)
+        {
+            return sayi1+sayi2;
+        }
+        #endregion
+
+        #region Soru 3
+        // 1 • den 10 • a kadar Olan sayılarının küplerinin toplamını bulan uygulamayı yazınız.
+        #endregion
+
+        #region Soru 4
+        //Doğw tarihi girilen kişinin yaşını hesaplayan uygulamayı yazınız.
+        #endregion
+
+        #region Soru5
+        //Giri1en sayının faktöriyelini hesaplayan uygulamayı yazınız.
+        #endregion
+    }
+}
