391e6839cf50bcfd09b4d58ca257d757b7be18af
oguzhandilek <oguzhandilek25@gmail.com>
2025-11-02 02:28:20 +0300

    ApplyConfigurationsFromAssembly  ve ApplyConfiguration işlendi

---
+++ Seperate_Configuration_Classes/Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+#region OnModelCreating
+//GeneL anlamda veritabanı ile iLgiLi konfigürasyoneL operasyonların dışında entityLer üzeirnde konfigürasyonel çalışmalar yapmamızı sağlayan bir fonskiyodundur.
+#endregion
+
+#region IEntityTypeConfiguration<T> Arayüzü
+//Entity bazlı yapılacak olan konfigürasyonları o entitye özel harici bir dosya üzerinde yapmamızı sağlayan bir arayüzdür.
+//Harici bir dosyada konfigürasyonların yürütülmesi merkezi bir yapılandırma noktası oluşturmamıızı sağlamaktadır.
+//Harici bir dosyada konfigüarsyonların yürüLtüLmesi entity sayısının fazla olduğu senaryolarda yönetilebilirliği artturacak ve yapılandırma ile ilgili geliştiricinin yükünü azaltacaktır.
+#endregion
+
+#region ApplyConfiguration Metodu
+//Bu metot harici konfigürasyoneL sınıflarımızı EF Core'a bildirebilmek için kullandığımız bir metotdur.
+#endregion
+
+#region ApplyConfigurationsFromAssembly Metodu
+//Uygulama bazında oluşturulan harici konfigürasyoneL sınıfların her birini OnMode1Creating metodunda ApplyConfiguration ile tek tek bildirmek yerine bu sınıfların bulunduğu Assembly'i bildirerek 1EntityTypeConfiguration arayüzünden türeyen tüm sınıfları ilgili entitye karşılık konfigürasyonel değer olarak baz almasını tek kalemde gerçekleştirmemizi sağlayan bir metottur.
+#endregion
+
+Console.WriteLine("Hello, World!");
+public class Order
+{
+    public int OrderId { get; set; }
+    public string Description { get; set; }
+    public DateTime OrderDate { get; set; }
+
+}
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(x => x.OrderId);
+        builder.Property(o => o.Description)
+            .HasMaxLength(13);
+        builder.Property(o => o.OrderDate)
+            .HasDefaultValueSql("GETDATE()");
+    }
+}
+
+public class ApplicationDbContext:DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ApplicationDb;Trusted_Connection=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.ApplyConfiguration(new OrderConfiguration());
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
