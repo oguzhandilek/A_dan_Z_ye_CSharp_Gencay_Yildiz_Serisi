bf9e1ad03a1adea5baf8b81422fb6f700baba44f
oguzhandilek <oguzhandilek25@gmail.com>
2025-11-01 23:15:22 +0300

    Generaed Values'a giriş yapıldı

---
+++ Generated_Values/Migrations/20251101200856_mig_1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Generated_Values.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Salary = table.Column<int>(type: "int", nullable: false, defaultValue: 100),
+                    Premium = table.Column<int>(type: "int", nullable: false),
+                    TotalGain = table.Column<int>(type: "int", nullable: false),
+                    PersonCode = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.PersonId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ Generated_Values/Program.cs
@@ -0,0 +1,71 @@
+﻿
+
+
+#region Generated Value Nedir?
+//EF Core' da üretilen değerlerle ilgili çeşitli modellerin ayrıntılarını yapılandırmamızı sağlayan bir konfigurasyondur.
+
+#endregion
+
+#region Default Values
+
+//EF Core herhangi bir tablonun herhangi bir kolonuna yazılım tarafından bir değer gönderilmediği taktirde bu kolona hangi değerin(default value) üretilip yazdırılacağını belirleyen yapılanmalardır.
+#region HasDefaultValue
+// Stat ic veri veriyor.
+#endregion
+
+#region HasDefaultValueSql
+//Sql cümleciği
+#endregion
+#endregion
+
+#region Computed Columns
+
+#endregion
+
+using Microsoft.EntityFrameworkCore;
+ApplicationDbContext context = new();
+
+Person person = new()
+{
+    Name="Oğuzhan",
+    Surname="Dilek",
+    Premium=10,
+    TotalGain=110,
+    PersonCode=1
+
+};
+await context.AddAsync(person);
+await context.SaveChangesAsync();
+Console.WriteLine("Hello, World!");
+public class Person
+{
+    public int PersonId { get; set; }
+
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    public int Salary { get; set; }
+    public int Premium { get; set; }
+    public int TotalGain { get; set; }
+    public int PersonCode { get; set; }
+}
+
+public class ApplicationDbContext:DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ApplicationDb;Trusted_Connection=True;");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>()
+            .HasKey(p => p.PersonId);
+        modelBuilder.Entity<Person>()
+            .Property(p => p.Salary)
+            .HasDefaultValue(100);
+            
+    }
+}
+
