778e6b1e73fb6983e74da1c6d30dc82e3ca901fb
oguzhandilek <oguzhandilek25@gmail.com>
2025-11-02 03:04:55 +0300

    Seed Datalar işlendi

---
+++ Data_Seeding/Migrations/20251101235603_mig_2.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Data_Seeding.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "BlogId", "Content", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, "...", "A" },
+                    { 2, 1, "...", "B" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
