989538c0dc653f23a4ad3fa22d76fc1e226d348c
oguzhandilek <oguzhan.dilek@jsga.edu.tr>
2025-12-04 08:33:26 +0300

    From sql işlendi ve Dynamic sql oluşturmadan devam

--- Sql_Queries/Program.cs
+++ Sql_Queries/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 ApplicationDbContext context = new();
@@ -11,7 +12,58 @@ ApplicationDbContext context = new();
 
 #region FromSqlInterpolated
 // EF Core 7.0 sürümünden önce ham sorguları execute edebildiğimiz fonksiyondur.
-var persons = await context.Persons.FromSqlInterpolated($"Select * From Persons").ToListAsync();
+//var persons = await context.Persons.FromSqlInterpolated($"Select * From Persons").ToListAsync();
+#endregion
+
+#region FromSql - EF Core 7.0
+//EF Core 7.0 ile gelen metottur.
+
+#region Query Execute
+//var persons= await context.Persons.FromSql($"select * from persons").ToListAsync();
+#endregion
+
+#region Stored Procedure Execute
+
+//var persons = await context.Persons.FromSql($"Execute dbo.usp_GetAllPersons null").ToListAsync();
+//var person = await context.Persons.FromSql($"Execute dbo.usp_GetAllPersons 4").ToListAsync();
+#endregion
+
+#region Parametreli Sorgu Oluşturma
+#region Ornek 1
+//var personId = 3;
+//var person= await context.Persons.FromSql($"select * from persons where Id={personId}").ToListAsync();
+#endregion
+#region Ornek 2
+//int personId = 2;
+//var persons= await context.Persons.FromSql($"execute usp_GetAllPersons {personId}").ToListAsync();
+
+// Burada sorguya geçirilen personld değişkeni arkaplanda bir DbParameter türüne dönüştürülerek o şekilde sorguya dahil edilmektedir.
+#endregion
+#region Ornek 3
+//SqlParameter personId = new("Id", "4");
+//personId.DbType=System.Data.DbType.Int32;
+//personId.Direction=System.Data.ParameterDirection.Input;
+
+//var persons= await context.Persons.FromSql($"execute usp_GetAllPersons {personId}").ToListAsync();
+
+#endregion
+#region Ornek 4
+//SqlParameter sqlParameter = new("Name", "Cucume");
+//sqlParameter.DbType=System.Data.DbType.String;
+
+//var personNames = await context.Persons.FromSql($"Select * from persons where Name={sqlParameter}").ToListAsync();
+#endregion
+#region Ornek 5
+//SqlParameter personId = new("Id", "1");
+//personId.DbType=System.Data.DbType.Int32;
+//var persons= await context.Persons.FromSql($"Execute usp_GetAllPersons @Id={personId}").ToListAsync();
+//@ ile parametre isimlerini vererek birden fazla parametreye sahip olan procedurceler için  ilgili parametreye doğru veriyi göndermemizi sağlamış oluruz.
+#endregion
+#endregion
+#endregion
+
+#region Dynamic SQL Oluşturma ve Parametre Girme
+
 #endregion
 
 
@@ -49,6 +101,6 @@ public class ApplicationDbContext:DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SqlQueries;Trusted_Connection=True;");
+        optionsBuilder.UseSqlServer("Server=(localdb)\\local;Database=SqlQueries;Trusted_Connection=True;");
     }
 }
