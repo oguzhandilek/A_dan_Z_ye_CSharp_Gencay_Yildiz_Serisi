6b379ab6029e3a6017a097756c4743acffc3ec50
oguzhandilek <oguzhandilek25@gmail.com>
2025-12-07 02:43:54 +0300

    Geriye Değer Döndüren Stored Proc Kullanma kadar işlendi Stored Procedure konus deva ediyor

---
+++ Stored_Procedures/Migrations/20251206231030_mig_1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Stored_Procedures.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonId = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Persons",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Muhiyittin" },
+                    { 2, "Sedrettin" },
+                    { 3, "Mugime" },
+                    { 4, "Cucume" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "Id", "Description", "PersonId" },
+                values: new object[,]
+                {
+                    { 1, "...", 1 },
+                    { 2, "...", 1 },
+                    { 3, "...", 2 },
+                    { 4, "...", 2 },
+                    { 5, "...", 2 },
+                    { 6, "...", 3 },
+                    { 7, "...", 4 },
+                    { 8, "...", 4 },
+                    { 9, "...", 3 },
+                    { 10, "...", 3 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_PersonId",
+                table: "Orders",
+                column: "PersonId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ Stored_Procedures/Program.cs
@@ -0,0 +1,116 @@
+﻿
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+ApplicationDbContext context = new();
+
+#region Stored Prodecure Nedir?
+//---- Stored Procedures (Saklı Yordamlar)
+//------ Genel Özelliklerı
+
+//----Normal sorgulardan daha hızlı çalışırlar.
+//----Çünkü normal sorgular Execute edilirken "Execute Plan” işlemi yapılır. Bu işlem sırasında hangi tablodan veri çekilecek, hangi kolonlardan gelecek, bunlar nerede v.s gibi işlemler yapılır. Bir sorgu her çalıştırıldığında bu işlemler aynen tekrar tekrar yapılır. Fakat sorgu Stored Procedure olarak çalıştırılırsa bu işlem sadece bir kere yapılır ve o da ilk çalıştırma esnasındadır. Diğer çalıştırmalarda bu işlemler yapılmaz. Bundan dolayı hız ve performansta artış sağlanır.
+//----İçerisinde Select, insert, Update ve Delete işlemleri yapılabilir.
+//----iç içe kullanılabilir.
+//----İçlerinde fonksiyon olutşrulabilir.
+//----- Sorgularımızın dışarıdan alacağı değerler parametre olarak Stored Procedure' lere geçirilebildiğinden dolayı, sorgularımızın "SQL Injection” yemelerinide önlemiş oluruz. Bu yönleriyle de daha güvenlidirler.
+//----- Stored Procedure fiziksel bir veritabanı nesnesidir. Haliyle Create komutu ile oluşturulur.
+//----- Fiziksel olarak ilgili veritabanının "Programmability” "Stored Procedures" kombinasyonundan erişilebilirler.
+
+//--Prototip -
+//--- Create Proc ya da Procedure [İsim]
+//--(
+//--varsa parametreler
+//--)
+//--as
+//--yazılacak sorgular, kodlar, şartlar,fonksiyonlar,komutlar---- Stored Procedures (Saklı Yordamlar)
+//------ Genel Özelliklerı
+
+//----Normal sorgulardan daha hızlı çalışırlar.
+//----Çünkü normal sorgular Execute edilirken "Execute Plan” işlemi yapılır. Bu işlem sırasında hangi tablodan veri çekilecek, hangi kolonlardan gelecek, bunlar nerede v.s gibi işlemler yapılır. Bir sorgu her çalıştırıldığında bu işlemler aynen tekrar tekrar yapılır. Fakat sorgu Stored Procedure olarak çalıştırılırsa bu işlem sadece bir kere yapılır ve o da ilk çalıştırma esnasındadır. Diğer çalıştırmalarda bu işlemler yapılmaz. Bundan dolayı hız ve performansta artış sağlanır.
+//----İçerisinde Select, insert, Update ve Delete işlemleri yapılabilir.
+//----iç içe kullanılabilir.
+//----İçlerinde fonksiyon olutşrulabilir.
+//----- Sorgularımızın dışarıdan alacağı değerler parametre olarak Stored Procedure' lere geçirilebildiğinden dolayı, sorgularımızın "SQL Injection” yemelerinide önlemiş oluruz. Bu yönleriyle de daha güvenlidirler.
+//----- Stored Procedure fiziksel bir veritabanı nesnesidir. Haliyle Create komutu ile oluşturulur.
+//----- Fiziksel olarak ilgili veritabanının "Programmability” "Stored Procedures" kombinasyonundan erişilebilirler.
+
+//--Prototip -
+//--- Create Proc ya da Procedure [İsim]
+//--(
+//--varsa parametreler
+//--)
+//--as
+//--yazılacak sorgular, kodlar, şartlar,fonksiyonlar,komutlar
+#endregion
+
+#region EF Core İle Stored Procedure Kullanımı
+
+#region Stored Procedure Oluşturma
+// 1. adım . boş bir migration oluşturunuz .
+// 2. adım . migration 'ın içerisindeki Up fonksiyonuna SPİ ın Create komutlarını yazınız, Down Fonk. ise Drop komutlarını yaznınz
+//3 Adım : migarate ediniz
+#endregion
+#region Stored Procedure Kullanma
+//SPII kullanabilmek için bir entityle ihtiyacımız vardır. Bu entity' nin DbSet propertysi ol larak context nesnesine de eklenmesi gerekmektedir. 
+//Bu DbSet properytysi üzerinden FromSqI fonksiyonunu kullanarak 'Exec ....' yapılanmasını çalıştırıp sorgu neticesini elde edebilirsiniz . komutu eşliğinde SP
+#region FromSql
+//var datas= await context.PersonOrders.FromSql($"execute usp_PersonOrders").ToListAsync();
+#endregion
+#endregion
+
+#region Geriye Değer Döndüren Stored Proc Kullanma
+
+#endregion
+
+#region Parametreli Stored Proc Kullanma
+
+#endregion
+
+#endregion
+
+Console.WriteLine("Hello, World!");
+
+
+public class Person
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+
+    public ICollection<Order> Orders { get; set; } = new List<Order>();
+
+
+}
+
+public class Order
+{
+    public int Id { get; set; }
+    public int PersonId { get; set; }
+    public string? Description { get; set; }
+
+    public Person Person { get; set; } = default!;
+
+}
+
+public class PersonOrder
+{
+    public string Name { get; set; }
+    public int Count { get; set; }
+}
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<PersonOrder> PersonOrders { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        modelBuilder.Entity<PersonOrder>()
+            .HasNoKey();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=StoredProcDb;Trusted_Connection=True;");
+    }
+}
