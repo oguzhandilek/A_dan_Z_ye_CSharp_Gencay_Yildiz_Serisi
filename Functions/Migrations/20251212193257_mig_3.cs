c6e88db87bbc9cfb7cdc2b822b621e078f96959d
oguzhandilek <oguzhandilek25@gmail.com>
2025-12-12 23:10:15 +0300

    Functions kosnus işlendi.

---
+++ Functions/Migrations/20251212193257_mig_3.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Functions.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"create function tvf_bestSellingStaff(@totalOrderPrice int)
+                                    returns table
+                                    as
+                                    Return
+                                    select top 1 p.Name
+                                    ,Count(*) OrderCount
+                                    ,Sum(o.Price) TotalOrderPrice
+                                    from Persons p
+                                    left join Orders o
+                                    on p.Id=o.PersonId
+                                    group by p.Name
+                                    having SUm(o.Price)<@totalOrderPrice
+                                    order by OrderCount desc");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"drop function tvf_bestSellingStaff");
+        }
+    }
+}
--- Functions/Program.cs
+++ Functions/Program.cs
@@ -14,23 +14,31 @@ ApplicationDbContext context = new();
 #endregion
 #region Scalar Function'u EF Core'a Entegre Etme
 #region HasDbFunction
-
+// Veritabanı seviyesindeki herhangi bir fonksiyonu EF Core/yazıLım kısmında bir metoda bind etmemizi sağlayan fonksiyondur.
 #endregion
 
-var persons= await (from person in context.Persons
-             where context.GetPersonTotalOrderPrice(person.Id)>500
-             select person).ToListAsync();
-Console.WriteLine("");
+//var persons = await (from person in context.Persons
+//             where context.GetPersonTotalOrderPrice(person.Id)>500
+//             select person).ToListAsync();
+//Console.WriteLine("");
 #endregion
 
 #region Inline Functions Nedir?
-
+//Geriye bir değer değil, tablo döndüren fonksiyonlardır.
 #endregion
 #region Inline Function Oluşturma
-
+//1.Adım: Bpş bir migration oluşturulacak
+//2.Adım : bu migration içerisinde Up metodunda Sql metodu eşliğinde fonksiyonun create kodları yazılacak, Down metodu içerisinde de Drop kodları yazılacaktır.
+//3.Adım: Migrate edilmeli
 #endregion
 #region Inline Function'u  EF Core'a Entegre Etme
-
+var person1= await context.BestSellingStaff().ToListAsync();
+var persons= await context.BestSellingStaff(500).ToListAsync();
+foreach (var person in persons)
+{
+    Console.WriteLine($"Name: {person.Name} | OrderCount: {person.OrderCount} | TotalPrice: {person.TotalOrderPrice}");
+}
+Console.WriteLine();
 #endregion
 Console.WriteLine("Hello, World!");
 public class Person
@@ -53,32 +61,42 @@ public class Order
     public Person Person { get; set; } = default!;
 
 }
+public class BestSellingStaff
+{
+    public string? Name { get; set; }
+    public int OrderCount { get; set; }
+    public int TotalOrderPrice { get; set; }
+}
 
-public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : DbContext
 {
     public DbSet<Order> Orders { get; set; }
     public DbSet<Person> Persons { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Server=(localdb)\\local;Database=FunctionDb;Trusted_Connection=True;");
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=FunctionDb;Trusted_Connection=True;");
     }
     #region Scalar Functions
     public int GetPersonTotalOrderPrice(int personId)
         => throw new Exception();
     #endregion
-
+    public IQueryable<BestSellingStaff> BestSellingStaff(int totalOrederPrice=1000)
+        => FromExpression(() => BestSellingStaff(totalOrederPrice));
     #region Inline Functions
 
     #endregion
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         #region Scalar
         modelBuilder.HasDbFunction(typeof(ApplicationDbContext).GetMethod(nameof(ApplicationDbContext.GetPersonTotalOrderPrice), new[] {typeof(int)}))
             .HasName("ufn_getPersonTotalOrderPrice");
         #endregion
         #region Inline
-
+        modelBuilder.HasDbFunction(typeof(ApplicationDbContext).GetMethod(nameof(ApplicationDbContext.BestSellingStaff), new[] { typeof(int) })).HasName("tvf_bestSellingStaff");
+        modelBuilder.Entity<BestSellingStaff>()
+            .HasNoKey();
         #endregion
-        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            
     }
 }
