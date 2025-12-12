35987309a91f07924537ffea517e68659a3c0fdd
oguzhandilek <oguzhandilek25@gmail.com>
2025-12-12 23:43:50 +0300

    EFCoreAdoNet Başlandı

---
+++ EFCoreAdoNet/Program.cs
@@ -0,0 +1,66 @@
+﻿
+
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+ApplicationDbContext context = new();
+
+#region Database Property'si
+//Database property'si veritabanını temsil eden ve EF Core'un bazı işlevlerinin detaylarına erişmemizi sağlayan bir propertydir.
+#endregion
+#region BeginTransaction
+//EF Core, transaction yönetimini otomatik bir şekilde kendisi gerçekleştirmektedir. Eğer ki transaction yönetimini manuel- olarak anlık ele almak isti orsak BeginTransaction fonksionunu kullanabiliriz.
+
+IDbContextTransaction transaction = context.Database.BeginTransaction();
+
+#endregion
+
+#region CommitTransaction
+//EF Core üzerinde yapılan çalışmaların commit edilebilmesi için kullanılan bir fonksiyondur.
+//context.Database.CommitTransaction();
+#endregion
+
+#region RollbackTransaction
+//EF Core üzerinde yapılan çalışmaların rollback edilebilmesi için kullanılan bir fonksiyondur.
+context.Database.RollbackTransaction();
+#endregion
+
+#region CanConnect
+bool connect= context.Database.CanConnect();
+#endregion
+
+Console.WriteLine("Hello, World!");
+
+public class Person
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+
+    public ICollection<Order> Orders { get; set; } = new List<Order>();
+
+
+}
+
+public class Order
+{
+    public int Id { get; set; }
+    public int PersonId { get; set; }
+    public string? Description { get; set; }
+    public int Price { get; set; }
+
+    public Person Person { get; set; } = default!;
+
+}
+
+public class ApplicationDbContext:DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EfCoreAdonetDb;Trusted_Connection=True;");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
