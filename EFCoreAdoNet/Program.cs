c892fbba2e5c9b638198b9e558b0b6715a60c782
oguzhandilek <oguzhandilek25@gmail.com>
2025-12-14 23:48:54 +0300

    EFCoreAdoNet Konusu devam ediyor.

--- EFCoreAdoNet/Program.cs
+++ EFCoreAdoNet/Program.cs
@@ -12,7 +12,7 @@ ApplicationDbContext context = new();
 #region BeginTransaction
 //EF Core, transaction yönetimini otomatik bir şekilde kendisi gerçekleştirmektedir. Eğer ki transaction yönetimini manuel- olarak anlık ele almak isti orsak BeginTransaction fonksionunu kullanabiliriz.
 
-IDbContextTransaction transaction = context.Database.BeginTransaction();
+//IDbContextTransaction transaction = context.Database.BeginTransaction();
 
 #endregion
 
@@ -23,13 +23,41 @@ IDbContextTransaction transaction = context.Database.BeginTransaction();
 
 #region RollbackTransaction
 //EF Core üzerinde yapılan çalışmaların rollback edilebilmesi için kullanılan bir fonksiyondur.
-context.Database.RollbackTransaction();
+//context.Database.RollbackTransaction();
 #endregion
 
 #region CanConnect
-bool connect= context.Database.CanConnect();
+// Verilen connection string'e karşılık bağlantı kurulabilir bir veritabanı var mı yok mu bunun bilgisini bool türde veren bir fonksiyondur. 
+bool connect = context.Database.CanConnect();
 #endregion
 
+#region EnsureCreated
+//EF Core 'da tasarlanan veritabanını migration kullanmaksızın, runtime 'da yani kod üzerinde veritabanı sunucusuna inşa edebilmek için kullanılan bir fonksiyondur.
+//bool isCreated =context.Database.EnsureCreated();
+#endregion
+#region EnsureDeleted
+// İnşa edilmiş veritabanını runtime'da silmemizi sağlayan bir fonksiyondur.
+//bool isDeleted =context.Database.EnsureDeleted();
+#endregion
+
+#region GenerateCreateScript
+//Context nesnesinde yapılmış olan veritabanı tasarımı her ne ise ona uygun bir SQL Script' ini string olarak veren metottur.
+//var script = context.Database.GenerateCreateScript();
+//Console.WriteLine(script);
+#endregion
+
+#region ExecuteSql
+//Veritabanına yapılacak insert, Update ve Delete sorgularını yazdığımız bir metottur. Bu metot işlevsel olarak alacağı parametreleri SQL Injection saldırılarına karşı korumaktadır.
+
+//var name = "Sülhettin";
+//var result = context.Database.ExecuteSql($"Insert Person Values({name})");
+#endregion
+
+#region ExcuteSqlRaw
+//Veritabanına yapılacak insert, Update ve Delete sorgularını yazdığımız bir metottur. Bu metotta ise sorguyu SQL Injection saldırılarına karşı koruma görevi geliştirinin sorumluluğundadır.
+//var name = "Şeref";
+//var result=context.Database.ExecuteSqlRaw($"Insert Person values({name})");
+#endregion
 Console.WriteLine("Hello, World!");
 
 public class Person
