1a871c0589cda9dd988bdd955bff37e36bacaf1e
oguzhandilek <oguzhan.dilek@jsga.edu.tr>
2025-12-15 17:05:54 +0300

    Logging konusu na bşlandı

---
+++ EF_Core_Logging/Configuration/OrderConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core_Logging.Configuration;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder
+            .HasData(new Order[]
+                   {
+                new Order { Id = 1,PersonId = 1,Description="...",Price=200},
+                new Order { Id = 2,PersonId = 1,Description="...",Price=210},
+                new Order { Id = 3,PersonId = 2,Description="...",Price=220},
+                new Order { Id = 4,PersonId = 2,Description="...",Price=230},
+                new Order { Id = 5,PersonId = 2,Description="...",Price=240},
+                new Order { Id = 6,PersonId = 3,Description="...",Price=250},
+                new Order { Id = 7,PersonId = 4,Description="...",Price=260},
+                new Order { Id = 8,PersonId = 4,Description="...",Price=270},
+                new Order { Id = 9,PersonId = 3,Description="...",Price=270},
+                new Order { Id = 10,PersonId = 3,Description="...",Price=280},
+                   });
+    }
+}
---
+++ EF_Core_Logging/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+ApplicationDbContext context = new();
+#region Neden Loglama Yaparız?
+//ÇaLışan bir sistemin runtime 'da nasıl davranış gerçekleştirdiğini gözlemleyebilmek için log mekanizmalarından istifade ederiz.
+#endregion
+#region Neleri Loglarız?
+
+#endregion
+
+Console.WriteLine("Hello, World!");
+public class Person
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+
+    public ICollection<Order> Orders { get; set; } = new List<Order>();
+}
+
+public class Order
+{
+    public int Id { get; set; }
+    public int PersonId { get; set; }
+    public string? Description { get; set; }
+    public int Price { get; set; }
+
+    public Person Person { get; set; } = default!;
+
+}
+
+public class ApplicationDbContext:DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\local;Database=EfCoreLogging;Trusted_Connection=True;");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
