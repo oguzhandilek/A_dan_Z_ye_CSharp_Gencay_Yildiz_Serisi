689edb5741c120ce070fd95491a435abb6464d84
oguzhandilek <oguzhan.dilek@jsga.edu.tr>
2025-12-23 16:56:16 +0300

    Conenction Resilincy konusu devam deiyor

---
+++ Connection_Resiliency/Migrations/20251223134152_mig_1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Connection_Resiliency.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.PersonId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Person",
+                columns: new[] { "PersonId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Ahmet" },
+                    { 2, "Hikmet" },
+                    { 3, "Zehmet" },
+                    { 4, "Melehat" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
---
+++ Connection_Resiliency/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+ApplicationDbContext context = new();
+
+#region Connection Resiliency Nedir?
+//EF Core üzerinde yapılan veritabanı çalışmaları sürecinde ister istemez veritabanı bağlantısında kopuşlar/ kesintiler vs. meydana gelebilmektedir.
+
+//Connection Resiliency ile kopan bağlantıyı tekrar kurmak için gerekli tekrar bağlantı taleplerinde bulunabilir ve biryandan da execution strategy dediğimiz davranış modellerini belirleyerek bağlantıların kopması durumunda tekrar edecek olan sorguları baştan sona yeniden tetikleyebiliriz.
+#endregion
+
+#region EnableRetryOnFailure
+//Uygu1ama sürecinde veritabanı bağlantısı koptuğu taktirde bu yapılandırma sayesinde bağlantıyı tekrardan kurmaya çalışabiliyirouz .
+
+while (true)
+{
+    await Task.Delay(2000);
+   var persons= await context.Persons.ToListAsync();
+    persons.ForEach(p=> Console.WriteLine(p.Name));
+    Console.WriteLine("*****************");
+
+}
+
+
+#region MaxRetryCount
+// Yeniden bağlantı sağlanması durumunun kaç kere gerçekleştirlecğeini bildirmektedir.
+#endregion
+#region MaxRetryDelay
+
+#endregion
+#endregion
+
+#region Execution Strategies
+
+#endregion
+
+
+public class Person
+{
+    public int PersonId { get; set; }
+    public string? Name { get; set; }
+}
+public class ApplicationDbContext:DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\local;Database=ConnectionResiliencyDb;Trusted_Connection=True;",
+            builder=>
+            builder.EnableRetryOnFailure())
+            .LogTo(filter:(eventId,level)=> eventId.Id==CoreEventId.ExecutionStrategyRetrying,
+            logger:eventData=>
+            {
+                Console.WriteLine($"Bağlantı kurulmaktadır");
+            }
+            );
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+       modelBuilder.Entity<Person>()
+            .HasData(
+           new Person() {PersonId=1, Name="Ahmet"},
+           new Person() {PersonId=2, Name="Hikmet"},
+           new Person() {PersonId=3, Name="Zehmet"},
+           new Person() {PersonId=4, Name="Melehat"}
+            );
+    }
+}
