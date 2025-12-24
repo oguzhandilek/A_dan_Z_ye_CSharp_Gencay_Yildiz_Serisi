7f338a525c014ea3bc541d29d73ccdf8e8507793
oguzhandilek <oguzhan.dilek@jsga.edu.tr>
2025-12-24 17:04:25 +0300

    Value Conversions devam ediyor

---
+++ Value_Conversions/Migrations/20251224135217_mig_3.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Value_Conversions.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Maried",
+                table: "Persons",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Maried",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Maried",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Maried",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Maried",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Maried",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Persons",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Maried",
+                value: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Maried",
+                table: "Persons");
+        }
+    }
+}
--- Value_Conversions/Program.cs
+++ Value_Conversions/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System.Runtime.InteropServices;
 
 ApplicationDbContext context = new();
@@ -10,6 +11,7 @@ ApplicationDbContext context = new();
 // Ya da /
 // UPDATE yahut İNSERT sorgularında da yazılım üzerinden veritabanına gönderdiğimiz veriler üzerinde de dönüşümler yapabilir ve böylece fiziksel olarak da verileri manipüle edebiliriz.l
 #endregion
+
 #region Value Converter Kullanımı Nasıldır?
 //VaLue conversions özelliğini EF Core 'da ki Value Converter yapıları tarafından uygulayabilmekteyiz.
 
@@ -20,6 +22,7 @@ var persons= await context.Persons.ToListAsync();
 Console.WriteLine();
 #endregion
 #endregion
+
 #region Enum Değerler İle Value Converter Kullanımı
 //Normal durumlarda Enum türünde tutulan propertylerin veritabanındaki karşılıkları int olacak şekilde aktarımı gerçekleştirlimektedir.Value converter sayesinde enum türünden olan propertylerinde dönüşümlerini istediğimiz türlere sağlayarak hem ilgili kolonun türünü o türde ayarlayaiblir hem de enum üzerinden çalış sürecinde verişel dönüşümleri ilgili türde sağlayabiliriz .
 
@@ -30,6 +33,36 @@ Console.WriteLine();
 
 #endregion
 
+#region ValueConverter Sınıfı
+//ValueConverter sınıfı, verişel dönüşümlerideki çalışmaları/sorumlulukları üstlenebilecek bir sınıftır. 
+// Yani bu sınıfın instance'ı ile HasConvention fonksiyonun yapılan çalışmaları üstlenebilir ve direkt bu instance'ıd ilgili fonksiyona vererek dönüşümsel çalışmalarımızı -gerçekleştirebiliiriz.
+
+//var _person = await context.Persons.FindAsync(7);
+
+#endregion
+
+#region Custom ValueConverter Sınıfı
+//EF Core Ida verişel- dönüşümler için custom olarak converter sınıfları üretebilmekteyiz. Bunun için tek yapılması gereken custom sınıfının ValueConverter sınıfından miras almasını sağlamaktadır.
+//var _person = await context.Persons.FindAsync(7);
+#endregion
+
+#region Built-in Converters Yapıları
+//EF Core basit dönüşümler için kendi bünyesinde yerleşik convert sınıfları barındırmaktadır.
+
+#region BoolToZeroOneConverter
+//bool olan verinin int olarak tutulmasını sağlar.
+#endregion
+#region BoolToStringConverter
+//bool olan verinin string olarak tutulmasını sağlar.
+#endregion
+#region BoolToTwoValuesConverter
+//bool olan verinin char olarak tutulmasını sağlar.
+#endregion
+
+//Diğer buiLt—in converters yapılarını aşağıdaki Linkten gözlemleyebilirsiniz.
+//https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/numeric-conversions
+#endregion
+
 Console.WriteLine("Hello, World!");
 
 public class Person
@@ -38,11 +71,25 @@ public class Person
     public string? Name { get; set; }
     public string? Gender { get; set; }
     public Gender Gender2 { get; set; }
+    public bool Maried { get; set; }
 }
 public enum Gender
 {
     Male,
     Female
+}
+
+public class GenderConverter:ValueConverter<Gender,string>
+{
+    public GenderConverter():base(
+        //INSERT-UPDATE
+        g=> g.ToString()
+        ,
+        //SELECT
+        g=> (Gender)Enum.Parse(typeof(Gender),g))
+    {
+        
+    }
 }
 public class ApplicationDbContext:DbContext
 {
@@ -57,12 +104,12 @@ public class ApplicationDbContext:DbContext
         modelBuilder.Entity<Person>().HasKey(p=> p.Id);
         modelBuilder.Entity<Person>().HasData(new Person[]
         {
-            new(){Id = 1,Name="Abdulhamit",Gender="M",Gender2=Gender.Male},
-            new(){Id = 2,Name="Eşref",Gender="M",Gender2=Gender.Male},
-            new(){Id = 3,Name="Bartu",Gender="M",Gender2=Gender.Male},
-            new(){Id = 4,Name="Cucume",Gender="F",Gender2=Gender.Female},
-            new(){Id = 5,Name="Halime",Gender="F",Gender2=Gender.Female},
-            new(){Id = 6,Name="Keriye",Gender="F",Gender2=Gender.Female},
+            new(){Id = 1,Name="Abdulhamit",Gender="M",Gender2=Gender.Male,Maried=true},
+            new(){Id = 2,Name="Eşref",Gender="M",Gender2=Gender.Male,Maried=true},
+            new(){Id = 3,Name="Bartu",Gender="M",Gender2=Gender.Male,Maried=true},
+            new(){Id = 4,Name="Cucume",Gender="F",Gender2=Gender.Female,Maried=true},
+            new(){Id = 5,Name="Halime",Gender="F",Gender2=Gender.Female,Maried=false},
+            new(){Id = 6,Name="Keriye",Gender="F",Gender2=Gender.Female,Maried=true},
         });
         #region Value Converter Kullanımı Nasıldır?
 
@@ -78,15 +125,48 @@ public class ApplicationDbContext:DbContext
         #endregion
 
         #region Enum Değerler İle Value Converter Kullanımı
-        modelBuilder.Entity<Person>().Property(p => p.Gender2)
-            .HasConversion(
-            //UPDATE-INSERT
-            g => g.ToString()
-            //g=> (int)g
-            ,
-            //SELECT
-            g=> (Gender)Enum.Parse(typeof(Gender),g)
-            );
+        //modelBuilder.Entity<Person>().Property(p => p.Gender2)
+        //    .HasConversion(
+        //    //UPDATE-INSERT
+        //    g => g.ToString()
+        //    //g=> (int)g
+        //    ,
+        //    //SELECT
+        //    g=> (Gender)Enum.Parse(typeof(Gender),g)
+        //    );
+        #endregion
+
+        #region ValueConverter Sınıfı
+        //ValueConverter<Gender, string> valueConverter = new(
+        //    //INSERT-UPDATE
+        //    g=> g.ToString()
+        //    ,
+        //    //SELECT
+        //    g=> (Gender)Enum.Parse(typeof(Gender),g)
+        //    );
+
+        //modelBuilder.Entity<Person>().Property(p=> p.Gender2).HasConversion(valueConverter);
+        #endregion
+
+        #region Custom ValueConverter Sınıfı
+        //modelBuilder.Entity<Person>().Property(p => p.Gender2).HasConversion<GenderConverter>();
+        #endregion
+
+        #region BoolToZeroOneConverter
+        //modelBuilder.Entity<Person>().Property(P => P.Maried).HasConversion<BoolToZeroOneConverter<int>>(); //1. seçenek
+        ////ya da direkt aşağıdaki gibi int türünü bildirirsek de aynı davranış söz konusu olacaktır.
+        //modelBuilder.Entity<Person>().Property(P => P.Maried).HasConversion<int>();//2.seçenek
+        #endregion
+
+        #region BoolToStringConverter
+        //BoolToStringConverter converter = new("Bekar", "Evli");
+        //modelBuilder.Entity<Person>().Property(P => P.Maried).HasConversion(converter);
+
+        #endregion
+
+        #region BoolToTwoValuesConverter
+        //BoolToTwoValuesConverter<char> converter = new('B', 'E');
+        //modelBuilder.Entity<Person>().Property(P => P.Maried).HasConversion(converter);
         #endregion
 
     }
